78d418070c51e63f8a5ad7854119e9d8455bde15
Yung-Hsiang Hu <iftnt1999@gmail.com>
2025-09-19 20:33:36 +0800

    [tool] fix: tool example

--- src/tools/example.py
+++ src/tools/example.py
@@ -6,14 +6,9 @@ import argparse
 import logging
 
 
-def main(**kwargs):
+def main(input_line, **kwargs):
     print(kwargs)
-    for line in fileinput.input():
-        try:
-            line = line.strip()
-            print(line)
-        except Exception as e:
-            print(f"{type(e).__name__}: {e.args[0]}")
+    print(input_line)
 
 
 def parse_args():
@@ -27,6 +22,12 @@ if __name__ == "__main__":
     args, unknown_args = parse_args()
     sys.argv = sys.argv[:1]
     logging.basicConfig(level=logging.INFO if not args.debug else logging.DEBUG)
-    if args.debug:
+    if not args.debug:
         sys.tracebacklimit = -1
-    main(**vars(args))
+
+    for line in fileinput.input():
+        try:
+            line = line.strip()
+            main(input_line=line, **vars(args))
+        except Exception as e:
+            print(f"{type(e).__name__}: {e.args[0]}")
